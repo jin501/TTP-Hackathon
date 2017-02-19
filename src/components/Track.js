a470b172384a61a315de93f051984a9999fa5f4e
jin501 <soljin01@gmail.com>
2017-02-19 02:35:46 -0500

    Menu

--- src/components/Track.js
+++ src/components/Track.js
@@ -5,10 +5,16 @@ import CourseItem from './CourseItem'
 // const courseLength = '50 hours'
 // const courseDescription = 'Course Description'
 class Track extends Component {
+  constructor(){
+    super();
+  }
   render(){
+    let courses = {
+
+    }
     return (
       <div className="Track">
-        <CourseItem props={this.courseName="Ruby"}/>
+        <CourseItem props={this.courseName}/>
       </div>
     )
   }
