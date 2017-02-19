c15e6ade4cf2dbb3c37671b33e861e57f213afb5
aehmt <calisahmet@gmail.com>
2017-02-19 02:24:44 -0500

    track comp

--- src/components/Track.js
+++ src/components/Track.js
@@ -3,9 +3,11 @@ import CourseItem from './CourseItem'
 
 class Track extends Component {
   render(){
+    const data = [{"course_name":"Ruby", "course_source":"www.source.com", "course_length":"50 hours", "course_description":"Course description"},]
+    const courses = data ? data.map((e,i) => ( <CourseItem courseName={e["course_name"]} /> )) : [];
     return (
       <div className="Track">
-        <CourseItem />
+        {courses}
       </div>
     )
   }
