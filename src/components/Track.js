c4c8fd348d711c69f98113c6cc76a41d465a56ad
jin501 <soljin01@gmail.com>
2017-02-19 02:15:51 -0500

    Add menu bar

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import CourseItem from './CourseItem'
-
+// const courseName = 'Ruby'
+// const courseSource = 'www.source.com'
+// const courseLength = '50 hours'
+// const courseDescription = 'Course Description'
 class Track extends Component {
   render(){
     return (
       <div className="Track">
-        <CourseItem />
+        <CourseItem props={this.courseName="Ruby"}/>
       </div>
     )
   }
