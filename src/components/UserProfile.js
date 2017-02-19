f0d8fef41687fdc2f9818be8e08a5eaeff897b73
jin501 <soljin01@gmail.com>
2017-02-18 22:18:06 -0500

    Basic components template

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import CourseList from './CourseList'
 
 class UserProfile extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {
+        name: '',
+        username: '',
+      },
+      course: {
+        
+      }
+    }
+  }
   render(){
     return (
       <div className="UserProfile">
