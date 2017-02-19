3c69833e9c4b9c50fa4bd5c26d0f7d04310160a3
jin501 <soljin01@gmail.com>
2017-02-19 01:43:38 -0500

    Add style to course container

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import UserProfile from './UserProfile'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="flex-container">
         <UserProfile />
       </div>
     );
