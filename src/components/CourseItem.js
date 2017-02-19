31f09f4b7a8a47e5ae950387003d7e3d788853f0
aehmt <calisahmet@gmail.com>
2017-02-19 01:33:07 -0500

    course list component semantic ui

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Image as ImageComponent, Item } from 'semantic-ui-react'
+
+const paragraph = <ImageComponent src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
 
 class CourseItem extends Component {
   render(){
     return (
       <div className="CourseList">
-        <h1>hi</h1>
+				<Item.Group>
+					<Item>
+						<Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+						<Item.Content>
+							<Item.Header>Arrowhead Valley Camp</Item.Header>
+							<Item.Meta>
+								<span className='price'>$1200</span>
+								<span className='stay'>1 Month</span>
+							</Item.Meta>
+							<Item.Description>paragraph</Item.Description>
+						</Item.Content>
+					</Item>
+				</Item.Group>
       </div>
     )
   }
 }
 
 export default CourseItem;
+
