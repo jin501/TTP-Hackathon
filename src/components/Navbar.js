67df9bbca611b1753e2fcc61b72b812af0bd4c29
Derick Quartey <niikwartey1@gmail.com>
2017-02-19 21:14:30 -0500

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Input, Menu, Image, Icon, Button } from 'semantic-ui-react';
 import { Link } from 'react-router';
 
+const logoUrl = 'https://static1.squarespace.com/static/57e01ab38419c2abeec91156/t/582b5bbf6b8f5b751a0d28e6/1479236544884/TTP_Site_Icons_Web.png';
 export default class Navbar extends Component {
   state = { activeItem: 'Home' }
 
@@ -11,8 +12,10 @@ export default class Navbar extends Component {
     const { activeItem } = this.state
 
     return (
-      <div>
-        <Image size='tiny' shape='circular' src='http://www.nyc.gov/html/techjobs/html/img/icon1.png' />
+      <div className='navbar'>
+        <div className='logo-wrapper'>
+          <Image className="logo" size='tiny' src={logoUrl} />
+        </div>
 
         <Menu icon inverted vertical borderless fixed='right' compact>
           <Menu.Item name='user' active={activeItem === 'user'} onClick={this.handleItemClick}>
